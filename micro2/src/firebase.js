07259cf3cadb0a43b68d3dbe4759cbd34258f61d
Valeriacarmonaf <97414518+Valeriacarmonaf@users.noreply.github.com>
2024-03-09 23:14:31 -0400

    Estructura

---
+++ micro2/src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCU3GhvsGlhEJHx6JFaMKykLZXHC-gAX_g",
+  authDomain: "microproyecto2carmonas.firebaseapp.com",
+  projectId: "microproyecto2carmonas",
+  storageBucket: "microproyecto2carmonas.appspot.com",
+  messagingSenderId: "201986506184",
+  appId: "1:201986506184:web:a8810730f80ced629514fd"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
